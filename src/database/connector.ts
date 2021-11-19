b9b17fc03f1bf01a654d0f17fec03a0692c75621
João Souza <joao@consultorweb.cnt.br>
2021-11-19 08:33:20 -0300

    Changes

--- src/database/connector.ts
+++ src/database/connector.ts
@@ -1,7 +1,24 @@
 import { Sequelize } from 'sequelize';
 import { ConfigService } from '@nestjs/config';
+import * as mysql from 'mysql2';
+
+const databaseExists = async (configService: ConfigService): Promise<void> => {
+  const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: configService.get('DATABASE_USERNAME'),
+    password: configService.get('DATABASE_PASSWORD'),
+  });
+  await mysqlConnection
+    .promise()
+    .query(`create database if not exists ${configService.get('DATABASE')}`);
+};
 
 const Connector = (configService: ConfigService): Sequelize => {
+  databaseExists(configService)
+    .then(() => {
+      return;
+    })
+    .catch((fail) => console.log(`failed: ${fail}`));
   const sequelize = new Sequelize(
     configService.get('DATABASE'),
     configService.get('DATABASE_USERNAME'),
@@ -12,9 +29,12 @@ const Connector = (configService: ConfigService): Sequelize => {
       logging: console.log,
     },
   );
-  sequelize.sync({ logging: false }).then(() => {
-    console.log('Sync done.');
-  });
+  sequelize
+    .sync({ logging: false })
+    .then(() => {
+      return;
+    })
+    .catch((fail) => console.log(`failed: ${fail}`));
   return sequelize;
 };
 export default Connector;
